5182707f00815bc642b77506c3b55c1af1516180
Tejas Mandwade <mandawadeteju@gmail.com>
2025-10-06 11:10:13 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,11 @@
+music = {   
+    "perfect": "https://www.youtube.com/watch?v=cNGjD0VG4R8",
+    "shaky": "https://www.youtube.com/watch?v=sUf2PtEZris",
+    "thunder": "https://www.youtube.com/watch?v=GtEvysh1654",
+    "unstoppable":"https://www.youtube.com/watch?v=oS07d8Gr4tw",
+    "jhingaat":"https://www.youtube.com/watch?v=5AeX7Ddq4ts",
+    "hindi":"https://www.youtube.com/watch?v=7dO_MS9tZ5E",
+    "abhi na jao": "https://www.youtube.com/watch?v=AB-I3vsUk6g",
+    "marathi ":"https://www.youtube.com/watch?v=9HNx1NIwdAc",
+    "marathi ":"https://www.youtube.com/watch?v=ultH9lO7y2s"  
+}
