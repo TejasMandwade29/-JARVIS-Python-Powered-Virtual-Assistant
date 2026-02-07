200fa603ee109d5f61976f26b93912ad707dcf38
Tejas Mandwade <mandawadeteju@gmail.com>
2026-02-07 09:25:07 +0530

    Replace old music list with new song URLs

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,11 +1,26 @@
-music = {   
-    "perfect": "https://www.youtube.com/watch?v=cNGjD0VG4R8",
-    "shaky": "https://www.youtube.com/watch?v=sUf2PtEZris",
-    "thunder": "https://www.youtube.com/watch?v=GtEvysh1654",
-    "unstoppable":"https://www.youtube.com/watch?v=oS07d8Gr4tw",
-    "jhingaat":"https://www.youtube.com/watch?v=5AeX7Ddq4ts",
-    "hindi":"https://www.youtube.com/watch?v=7dO_MS9tZ5E",
-    "abhi na jao": "https://www.youtube.com/watch?v=AB-I3vsUk6g",
-    "marathi ":"https://www.youtube.com/watch?v=9HNx1NIwdAc",
-    "marathi ":"https://www.youtube.com/watch?v=ultH9lO7y2s"  
+# Music library mapping song names to YouTube URLs
+music = {
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "despacito": "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "see you again": "https://www.youtube.com/watch?v=RgKAFK5djSk",
+    "uptown funk": "https://www.youtube.com/watch?v=OPf0YbXqDm0",
+    "workout": "https://www.youtube.com/watch?v=ZXsQAXx_ao0",
+    "gym music": "https://www.youtube.com/watch?v=yrP6n8f2q-c",
+    "study music": "https://www.youtube.com/watch?v=5gDLA6y-3x4",
+    "focus": "https://www.youtube.com/watch?v=5qap5aO4i9A",
+    "bollywood": "https://www.youtube.com/watch?v=FyXqT9gQNCA",
+    "hollywood": "https://www.youtube.com/watch?v=JGwWNGJdvx8&list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA",
+    "classical": "https://www.youtube.com/watch?v=4Tr0otuiQuU",
+    "jazz": "https://www.youtube.com/watch?v=0Fb-JwC_9C4",
+    "rock": "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+    "pop": "https://www.youtube.com/watch?v=JGwWNGJdvx8&list=RDCLAK5uy_mfLxb4-m_sSxH5Jg1mTcdVye2c6_Dx_CM",
+    
+    # Add more songs here
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "happier": "https://www.youtube.com/watch?v=m7Bc3pLyij0",
+    "senorita": "https://www.youtube.com/watch?v=Pkh8UtuejGw"
 }
+
+# You can add more songs in this format:
+# "song name": "youtube_url",
